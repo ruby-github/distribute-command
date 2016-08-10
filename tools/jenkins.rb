6dd4da2b375df342f7e009d8a3bf8b3b178ba8e0
10067748 <gou.yabin@zte.com.cn>
2016-08-10 13:44:35 +0800

    update

--- tools/jenkins.rb
+++ tools/jenkins.rb
@@ -1394,8 +1394,8 @@ if $0 == __FILE__
     build.bn_build ['release/20160606']
 
     # IPTN开发版本
-    #build.bn_build ['dev/20160314', 'dev/20160417_wdm', 'dev/20160524_MTN', 'dev/20160611', 'dev/20160704'], [:windows, :windows32]
-    #build.bn_build ['dev/20160627_MTN'], [:windows]
+    build.bn_build ['dev/20160314', 'dev/20160417_wdm', 'dev/20160524_MTN', 'dev/20160611', 'dev/20160704'], [:windows, :windows32]
+    build.bn_build ['dev/20160627_MTN'], [:windows]
 
     build.bn_build ['dev/20160801']
 
