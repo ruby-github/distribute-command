3a65b4c5cd0bcbf4f07c03220dd47dc832ad721c
admin <admin@git.com>
2016-08-17 16:57:20 +0800

    update

--- doc/bn/u3_template.rb
+++ doc/bn/u3_template.rb
@@ -2,83 +2,86 @@ require 'fileutils'
 require 'net/ftp'
 
 LOG_FILE = 'log.log'
+PATCH_HOME = 'c:/temp/u3_patch'
 
 if $0 == __FILE__
-  Dir.chdir 'c:/temp/u3_patch' do
-    if File.file? LOG_FILE
-      if File.size(LOG_FILE) > 102400
-        FileUtils.rm_rf LOG_FILE
+  if File.directory? PATCH_HOME
+    Dir.chdir PATCH_HOME do
+      if File.file? LOG_FILE
+        if File.size(LOG_FILE) > 102400
+          FileUtils.rm_rf LOG_FILE
+        end
       end
-    end
 
-    status = true
+      status = true
 
-    File.open LOG_FILE, 'a' do |f|
-      f.puts '%s %s %s' % ['=' * 30, Time.now, '=' * 30]
+      File.open LOG_FILE, 'a' do |f|
+        f.puts '%s %s %s' % ['=' * 30, Time.now, '=' * 30]
 
-      begin
-        Net::FTP.open '10.8.9.80', 'user', 'user' do |ftp|
-          Dir.glob('**/*.{xml,zip}').each do |file|
-            if not File.file? file
-              next
-            end
+        begin
+          Net::FTP.open '10.8.9.80', 'user', 'user' do |ftp|
+            Dir.glob('**/*.{xml,zip}').each do |file|
+              if not File.file? file
+                next
+              end
 
-            paths = file.split '/'
-            paths.shift
+              paths = file.split '/'
+              paths.shift
 
-            dest_file = File.join 'source', paths.join('/')
+              dest_file = File.join 'source', paths.join('/')
+              dir = nil
 
-            line = '%s ftp put file: %s -> %s' % [Time.now, file, dest_file]
+              paths = dest_file.split '/'
+              paths.pop
 
-            dir = nil
+              paths.each do |path|
+                if dir.nil?
+                  dir = path
+                else
+                  dir = File.join dir, path
+                end
 
-            paths = dest_file.split '/'
-            paths.pop
-
-            paths.each do |path|
-              if dir.nil?
-                dir = path
-              else
-                dir = File.join dir, path
+                begin
+                  ftp.mkdir dir
+                rescue
+                end
               end
 
               begin
-                ftp.mkdir dir
+                ftp.delete dest_file
               rescue
               end
-            end
 
-            begin
-              ftp.delete dest_file
-              ftp.putbinaryfile file , dest_file
+              begin
+                ftp.putbinaryfile file , dest_file
+                FileUtils.rm_rf file
 
-              FileUtils.rm_rf file
+                f.puts '[SUCCESS] %s ftp put file: %s -> %s' % [Time.now, file, dest_file]
+              rescue
+                f.puts '[FAILED ] %s ftp put file: %s -> %s' % [Time.now, file, dest_file]
 
-              f.puts '%s success' % line
-            rescue
-              f.puts '%s fail' % line
-
-              status = false
+                status = false
+              end
             end
           end
-        end
-      rescue
-        f.puts $!.to_s
+        rescue
+          f.puts $!.to_s
 
-        status = false
+          status = false
+        end
+
+        f.puts
       end
 
-      f.puts
-    end
-
-    if status
-      Dir.glob('*').each do |file|
-        if File.directory? file
-          FileUtils.rm_rf file
+      if status
+        Dir.glob('*').each do |file|
+          if File.directory? file
+            FileUtils.rm_rf file
+          end
         end
       end
-    end
 
-    status
+      status
+    end
   end
 end
