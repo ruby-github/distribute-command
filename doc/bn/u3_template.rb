83b22e701e99215ddfb530c6af9078b53906795f
admin <admin@git.com>
2016-08-18 12:13:36 +0800

    update

--- doc/bn/u3_template.rb
+++ doc/bn/u3_template.rb
@@ -1,8 +1,8 @@
 require 'fileutils'
-require 'net/ftp'
 
 LOG_FILE = 'log.log'
 PATCH_HOME = 'c:/temp/u3_patch'
+SOURCE_HOME = '//10.8.9.80/source'
 
 if $0 == __FILE__
   if File.directory? PATCH_HOME
@@ -13,61 +13,41 @@ if $0 == __FILE__
         end
       end
 
+      system "net use \\\\10.8.9.80\\source share /user:user"
+
       status = true
 
       File.open LOG_FILE, 'a' do |f|
         f.puts '%s %s %s' % ['=' * 30, Time.now, '=' * 30]
 
-        begin
-          Net::FTP.open '10.8.9.80', 'user', 'user' do |ftp|
-            Dir.glob('**/*.{xml,zip}').each do |file|
-              if not File.file? file
-                next
-              end
+        f.puts '%s %s %s' % ['=' * 30, Time.now, '=' * 30]
 
-              paths = file.split '/'
-              paths.shift
-
-              dest_file = File.join 'source', paths.join('/')
-              dir = nil
-
-              paths = dest_file.split '/'
-              paths.pop
-
-              paths.each do |path|
-                if dir.nil?
-                  dir = path
-                else
-                  dir = File.join dir, path
-                end
-
-                begin
-                  ftp.mkdir dir
-                rescue
-                end
-              end
-
-              begin
-                ftp.delete dest_file
-              rescue
-              end
-
-              begin
-                ftp.putbinaryfile file , dest_file
-                FileUtils.rm_rf file
-
-                f.puts '[SUCCESS] %s ftp put file: %s -> %s' % [Time.now, file, dest_file]
-              rescue
-                f.puts '[FAILED ] %s ftp put file: %s -> %s' % [Time.now, file, dest_file]
-
-                status = false
-              end
-            end
+        Dir.glob('**/*.{xml,zip}').each do |file|
+          if not File.file? file
+            next
           end
-        rescue
-          f.puts $!.to_s
 
-          status = false
+          paths = file.split '/'
+          paths.shift
+
+          dest_file = File.join SOURCE_HOME, paths.join('/')
+
+          file_locale = file.dup.encode 'locale', invalid: :replace, undef: :replace, replace: ''
+          dest_file_locale = dest_file.dup.encode 'locale', invalid: :replace, undef: :replace, replace: ''
+
+          begin
+            FileUtils.mkdir_p File.dirname(dest_file_locale)
+
+            FileUtils.copy_file file_locale, dest_file_locale, false
+
+            f.puts '[SUCCESS] %s copy file: %s ->' % [Time.now, file_locale]
+            f.puts ' ' * 10 + dest_file_locale
+          rescue
+            f.puts '[FAILED ] %s copy file: %s ->' % [Time.now, file_locale]
+            f.puts ' ' * 10 + dest_file_locale
+
+            status = false
+          end
         end
 
         f.puts
@@ -80,8 +60,6 @@ if $0 == __FILE__
           end
         end
       end
-
-      status
     end
   end
 end
